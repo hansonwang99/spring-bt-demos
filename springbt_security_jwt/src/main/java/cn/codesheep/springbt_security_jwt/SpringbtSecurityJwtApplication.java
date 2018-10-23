d151c7b29392acfb4bdfc73532a29ebfe820342a
hansonwang99 <hansonwang99@163.com>
2018-10-23 14:02:03 +0800

    init repo

---
+++ springbt_security_jwt/src/main/java/cn/codesheep/springbt_security_jwt/SpringbtSecurityJwtApplication.java
@@ -0,0 +1,12 @@
+package cn.codesheep.springbt_security_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbtSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbtSecurityJwtApplication.class, args);
+    }
+}
